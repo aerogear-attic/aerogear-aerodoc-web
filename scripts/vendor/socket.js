93c8ffab0b9b6ff5a4883cb21d5ea540ab92917b
Sebastien Blanc <scm.blanc@gmail.com>
2013-07-27 18:57:29 +0200

    switch to latest ag-js and refactor socket.js

--- scripts/vendor/socket.js
+++ scripts/vendor/socket.js
@@ -1,21 +1,22 @@
 (function() {
-    var mailEndpoint, mailRequest, fooEndpoint, fooRequest, broadcastRequest, broadcastEndpoint, testFrame;
+    var leadEndpoint, leadRequest, broadcastRequest, broadcastEndpoint,
+    UPClient = AeroGear.UnifiedPushClient( AeroGear.SimplePush.variantID, "0dcdc9ec-495c-4c2d-a675-b61facbb7247", "http://localhost:8080/ag-push/rest/registry/device" );
 
-    console.log("Web Socket opened!");
+  console.log("Web Socket opened!");
 
     broadcastRequest = navigator.push.register();
     broadcastRequest.onsuccess = function( event ) {
         broadcastEndpoint = event.target.result;
-        broadcastRequest.registerWithPushServer( "broadcast", broadcastEndpoint );
+        UPClient.registerWithPushServer.registerWithPushServer( "broadcast", broadcastEndpoint );
         console.log("Subscribed to Broadcast messages on " + broadcastEndpoint.channelID);
         console.log(localStorage.getItem( broadcastEndpoint.channelID ) || 1 );
        
     };
 
-    mailRequest = navigator.push.register();
-    mailRequest.onsuccess = function( event ) {
-        mailEndpoint = event.target.result;
-        mailRequest.registerWithPushServer( "lead", mailEndpoint , "maria" );
+  leadRequest = navigator.push.register();
+  leadRequest.onsuccess = function( event ) {
+        leadEndpoint = event.target.result;
+    UPClient.registerWithPushServer( "lead", mailEndpoint , "maria" );
        // $("#mailVersion").attr("name", mailEndpoint.channelID);
         console.log("Subscribed to lead messages on " + mailEndpoint.channelID);
         console.log(localStorage.getItem( mailEndpoint.channelID ) || 1 );
@@ -23,3 +24,4 @@
     };
        
 })();
+
