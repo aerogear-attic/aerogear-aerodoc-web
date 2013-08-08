ead455365175cb750f2a89573982b4b2d1096c54
Sebastien Blanc <scm.blanc@gmail.com>
2013-08-08 08:01:33 +0200

    back port the webinar changes

--- scripts/services/dataService.js
+++ scripts/services/dataService.js
@@ -17,12 +17,13 @@
 "use strict";
 
 aerodoc.factory("dataService", function() {
+	var serverUrl = aeroConfig.serverURL;
 	return {
 		restAuth : AeroGear.Auth({
 			name : "auth",
 			settings : {
 				agAuth : true,
-				baseURL : "http://localhost:8080/aerodoc/rest/",
+				baseURL : serverUrl + "/aerodoc/rest/",
 				endpoints : {
 					enroll : "register",
 					login : "login",
@@ -32,43 +33,18 @@ aerodoc.factory("dataService", function() {
 		}).modules.auth,
 		leadPipe : AeroGear.Pipeline({
 			name : "leads",
-			settings : {
-				baseURL : "http://localhost:8080/aerodoc/rest/",
+           	settings : {
+           		baseURL : serverUrl + "/aerodoc/rest/",
 				endpoint : "leads",
 				authenticator : this.restAuth
 			}
 		}).pipes.leads,
-
-		searchAgents : AeroGear.Pipeline({
-			name : "searchAgents",
-			settings : {
-				authenticator : this.restAuth,
-				endpoint: "rest/saleagents/searchagents",
-				baseURL : "http://localhost:8080/aerodoc/rest/"
-			}
-		}).pipes.searchAgents,
-
 		leadStore : AeroGear.DataManager({
 			name : "Lead",
 			type : "SessionLocal",
 			settings : {
 				storageType : "localStorage"
 			}
-		}).stores.Lead,
-		saleAgentPipe : AeroGear.Pipeline({
-			name : "rest/saleagents",
-			settings : {
-				authenticator : this.restAuth,
-				baseURL : "http://localhost:8080/aerodoc/rest/"
-			}
-		}).pipes.saleagents,
-
-		saleAgentStore : AeroGear.DataManager({
-			name : "SaleAgent",
-			type : "SessionLocal",
-			settings : {
-				storageType : "localStorage"
-			}
-		}).stores.SaleAgent
+		}).stores.Lead
 	};
 });
